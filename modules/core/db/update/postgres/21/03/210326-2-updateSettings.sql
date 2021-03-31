8884778439b459b385b9ca686cd7cf166deb56f6
Sergei <54173254+svgetm@users.noreply.github.com>
2021-03-31 10:40:48 +0300

    Delete modules/core/db/update/postgres/21/03 directory

--- modules/core/db/update/postgres/21/03/210326-2-updateSettings.sql
+++
@@ -1,3 +0,0 @@
-alter table SCANFOLDER_SETTINGS add column SCANING boolean ^
-update SCANFOLDER_SETTINGS set SCANING = false where SCANING is null ;
-alter table SCANFOLDER_SETTINGS alter column SCANING set not null ;
